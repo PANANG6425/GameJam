207a70431d2912f4b3bc339938f3995ee9896816
HeHHeyboi <thanathatwave@gmail.com>
2026-07-24 14:30:25 +0700

    finish throw project tile draw

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Throw : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    bool mouseHold = false;
+    readonly float defaultAngle = 45;
+    float currentAngle = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.sortingLayerID = SortingLayer.NameToID("Player");
+
+        currentAngle = defaultAngle;
+    }
+
+    public void OnLeftClick(InputAction.CallbackContext ctx)
+    {
+        switch (ctx.phase)
+        {
+            case InputActionPhase.Started:
+                lineRenderer.positionCount = 3;
+                mouseHold = true;
+                currentAngle = defaultAngle;
+                break;
+            case InputActionPhase.Canceled:
+                lineRenderer.positionCount = 0;
+                mouseHold = false;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseHold)
+        {
+            float radians = currentAngle * Mathf.Deg2Rad;
+            var mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector3 apex = transform.position;
+
+            float baseAngle = Mathf.Atan2(mousePos.y - apex.y, mousePos.x - apex.x);
+            apex.y += 1;
+            apex.x += 0.5f;
+
+            Vector3 upperDir = new(
+                Mathf.Cos(baseAngle + radians),
+                Mathf.Sin(baseAngle + radians),
+                0
+            );
+            Vector3 lowerDir = new(
+                Mathf.Cos(baseAngle - radians),
+                Mathf.Sin(baseAngle - radians),
+                0
+            );
+
+            lineRenderer.SetPosition(0, apex + upperDir * 5);
+            lineRenderer.SetPosition(1, apex);
+            lineRenderer.SetPosition(2, apex + lowerDir * 5);
+            currentAngle = (currentAngle <= 0) ? 0 : currentAngle - 1;
+        }
+    }
+}
